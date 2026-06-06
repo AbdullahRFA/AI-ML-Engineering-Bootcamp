67268833e03dfc956d2d46c2478382c884f6dba0
AbdullahRFA <shakibrybmn@gmail.com>
2026-06-06 20:22:38 +0600

    implement singleton

---
+++ important program/design_pattern/singleTon.py
@@ -0,0 +1,49 @@
+# class Singleton:
+#     _instance = None
+    
+#     def __new__(cls):
+#         if cls._instance is None:
+#             print("Instant Creating")
+#             cls._instance = super().__new__(cls)
+#             print("Instant Created")
+#         return cls._instance
+#     def getMessage(self):
+#         print("This is singleton instance")
+    
+# obj1 = Singleton()
+# obj2 = Singleton()
+
+# obj1.getMessage()
+# obj2.getMessage()
+
+# if obj1==obj2:
+#     print("Obj1 == obj2")
+#     print("Obj1 id = ",id(obj1))
+#     print("Obj2 id = ",id(obj2))
+# else:
+#     print("Obj1 != obj2")
+
+
+
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            print("Connecting to database...")
+            cls._instance = super().__new__(cls)
+            cls._instance.connection = "Database Connected"
+        return cls._instance
+
+    def get_connection(self):
+        return self.connection
+
+
+db1 = DatabaseConnection()
+db2 = DatabaseConnection()
+
+print(db1.get_connection())
+print(db2.get_connection())
+
+print(db1 is db2)  # True
+            
